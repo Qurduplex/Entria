d37ae477ccdbc8cc88ea7242c6e26cb1dc6460ce
Margiorno <m.zawiski03@gmail.com>
2026-03-24 15:03:01 +0100

    identity-service runs

---
+++ Entria-Backend/identity-service/src/main/java/edu/pk/qurduplex/identityService/IdentityServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.pk.qurduplex.identityService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityServiceApplication.class, args);
+	}
+}
