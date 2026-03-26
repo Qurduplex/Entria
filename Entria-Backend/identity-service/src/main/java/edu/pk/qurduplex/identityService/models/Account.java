c7d0908b8880b7bf59c26313340a09188274bb13
Margiorno <m.zawiski03@gmail.com>
2026-03-26 10:09:05 +0100

    account model(entity responsible for authentication)

---
+++ Entria-Backend/identity-service/src/main/java/edu/pk/qurduplex/identityService/models/Account.java
@@ -0,0 +1,26 @@
+package edu.pk.qurduplex.identityService.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String email;
+    private String passwordHash;
+}
