4ed4de3a59881c5268f5d669f1c1457f1743c460
Margiorno <m.zawiski03@gmail.com>
2026-04-22 13:52:33 +0200

    empty eureka server module

---
+++ Entria-Backend/eureka-server/src/main/java/edu/pk/qurduplex/eureka/EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package edu.pk.qurduplex.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
