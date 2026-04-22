541c6a6a993497cea8a598c314c2cbcd340b136d
Margiorno <m.zawiski03@gmail.com>
2026-04-22 15:24:12 +0200

    enabling eureka

--- Entria-Backend/identity-service/src/main/java/edu/pk/qurduplex/identityService/IdentityServiceApplication.java
+++ Entria-Backend/identity-service/src/main/java/edu/pk/qurduplex/identityService/IdentityServiceApplication.java
@@ -2,8 +2,10 @@ package edu.pk.qurduplex.identityService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IdentityServiceApplication {
 
 	public static void main(String[] args) {
