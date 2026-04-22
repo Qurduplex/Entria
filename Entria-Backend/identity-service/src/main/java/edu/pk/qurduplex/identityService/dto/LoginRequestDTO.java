fbe381a06f25152b4a7db3c6e3bf3fab7a3de1f3
Margiorno <m.zawiski03@gmail.com>
2026-04-22 23:10:50 +0200

    password checking(length etc) before register, not before login

--- Entria-Backend/identity-service/src/main/java/edu/pk/qurduplex/identityService/dto/LoginRequestDTO.java
+++ Entria-Backend/identity-service/src/main/java/edu/pk/qurduplex/identityService/dto/LoginRequestDTO.java
@@ -19,11 +19,6 @@ public class LoginRequestDTO {
     private String email;
 
     @NotBlank(message = "password is required")
-    @NotBlank(message = "password is required")
     @Size(min = 8, max = 32, message = "password must be between 8 and 32 characters long")
-    @Pattern(
-            regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-            message = "password must contain at least one uppercase letter, one number and one special character"
-    )
     private String password;
 }
