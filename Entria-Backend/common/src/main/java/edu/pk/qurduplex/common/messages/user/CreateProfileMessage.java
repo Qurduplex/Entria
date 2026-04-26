5fb0a0994ce6e95ef7b5310a55a90efdd715d8eb
Margiorno <m.zawiski03@gmail.com>
2026-04-26 16:40:28 +0200

    common module

---
+++ Entria-Backend/common/src/main/java/edu/pk/qurduplex/common/messages/user/CreateProfileMessage.java
@@ -0,0 +1,9 @@
+package edu.pk.qurduplex.common.messages.user;
+
+import java.util.UUID;
+
+public class CreateProfileMessage {
+    private UUID userId;
+    private String firstName;
+    private String lastName;
+}
