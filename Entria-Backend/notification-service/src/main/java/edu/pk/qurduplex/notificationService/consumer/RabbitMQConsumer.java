a97fefdaa83d72bd75e3a87b3f82cae8503391f1
Margiorno <m.zawiski03@gmail.com>
2026-04-27 15:43:46 +0200

    notification-service sends emails

--- Entria-Backend/notification-service/src/main/java/edu/pk/qurduplex/notificationService/consumer/RabbitMQConsumer.java
+++ Entria-Backend/notification-service/src/main/java/edu/pk/qurduplex/notificationService/consumer/RabbitMQConsumer.java
@@ -2,6 +2,7 @@ package edu.pk.qurduplex.notificationService.consumer;
 
 import edu.pk.qurduplex.common.messages.user.ResetPasswordCodeMessage;
 import edu.pk.qurduplex.common.messages.user.VerificationCodeMessage;
+import edu.pk.qurduplex.notificationService.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,17 +13,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RabbitMQConsumer {
 
+    private final EmailService emailService;
 
     @RabbitListener(queues = "#{rabbitMQProperties.getVerificationCodeEventTopic()}")
     public void processVerificationCodeMessage(VerificationCodeMessage message) {
         log.info("Received verification code message: {}", message);
 
+        emailService.sendVerificationEmail(message.getEmail(), message.getCode());
     }
 
     @RabbitListener(queues = "#{rabbitMQProperties.getResetPasswordCodeEventTopic()}")
     public void processResetPasswordCodeMessage(ResetPasswordCodeMessage message) {
         log.info("Received reset password code message: {}", message);
 
-
+        emailService.sendResetPasswordEmail(message.getEmail(), message.getCode());
     }
 }
