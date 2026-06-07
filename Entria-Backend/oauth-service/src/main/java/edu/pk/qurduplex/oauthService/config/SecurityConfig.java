2d7f2f91ae1efdfedb69e319ea883cc8661f9e87
Margiorno <m.zawiski03@gmail.com>
2026-06-07 13:11:16 +0200

    oauth service uses a postgredb instead of library inbuild in-memory db

--- Entria-Backend/oauth-service/src/main/java/edu/pk/qurduplex/oauthService/config/SecurityConfig.java
+++ Entria-Backend/oauth-service/src/main/java/edu/pk/qurduplex/oauthService/config/SecurityConfig.java
@@ -13,11 +13,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.web.SecurityFilterChain;
@@ -140,5 +144,13 @@ public class SecurityConfig {
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().build();
+    }
+
+    @Bean
+    public OAuth2AuthorizationService authorizationService(
+            JdbcTemplate jdbcTemplate,
+            RegisteredClientRepository registeredClientRepository) {
+
+        return new JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository);
     }
 }
--- Entria-Backend/oauth-service/src/main/java/edu/pk/qurduplex/oauthService/security/GrpcRegisteredAppRepository.java
+++ Entria-Backend/oauth-service/src/main/java/edu/pk/qurduplex/oauthService/security/GrpcRegisteredAppRepository.java
@@ -50,14 +50,39 @@ public class GrpcRegisteredAppRepository implements RegisteredClientRepository {
     }
 
     @Override
-    public void save(RegisteredClient registeredClient) {
-        // todo: maybe todo
-        throw new UnsupportedOperationException("Saving clients is handled by app-registry-service");
+    public RegisteredClient findById(String id) {
+        try {
+            AppResponse response = appStub.getApplicationById(
+                    edu.pk.qurduplex.common.grpc.AppIdRequest.newBuilder().setId(id).build()
+            );
+
+            return RegisteredClient.withId(response.getId())
+                    .clientId(response.getClientId())
+                    .clientSecret(response.getClientSecretHash())
+                    .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                    .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                    .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                    .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                    .redirectUri(response.getRedirectUri())
+                    .scopes(scopes -> {
+                        scopes.add(OidcScopes.OPENID);
+                        for (String permission : response.getPermissionsMap().keySet()) {
+                            scopes.add(permission.toLowerCase());
+                        }
+                    })
+                    .clientSettings(org.springframework.security.oauth2.server.authorization.settings.ClientSettings.builder()
+                            .requireAuthorizationConsent(true)
+                            .build())
+                    .build();
+
+        } catch (io.grpc.StatusRuntimeException e) {
+            return null;
+        }
     }
 
     @Override
-    public RegisteredClient findById(String id) {
+    public void save(RegisteredClient registeredClient) {
         // todo: maybe todo
-        return null;
+        throw new UnsupportedOperationException("Saving clients is handled by app-registry-service");
     }
 }
